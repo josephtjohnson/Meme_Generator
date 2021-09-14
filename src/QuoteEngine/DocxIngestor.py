482e9d701eae763e04550cc4d76f1d7924c38228
josephtjohnson <82980802+josephtjohnson@users.noreply.github.com>
2021-09-14 15:22:55 -0400

    Initial commit

---
+++ src/QuoteEngine/DocxIngestor.py
@@ -0,0 +1,22 @@
+import docx
+from typing import List
+from .IngestorInterface import Ingestor
+from .QuoteModel import QuoteModel
+
+class DocxIngestor(IngestorInterface):
+    file_types = []
+
+    @classmethod
+    def parse(cls, path: str) -> List[QuoteModel]:
+        if not cls.can_ingest(path):
+            return Exception(f'Cannot ingest this file type. Please ensure \
+            file extension is {file_types}')
+
+        quotes = []
+        doc = docx.Document(path)
+
+        for para in doc.paragraphs:
+            if para.text != "":
+                parse = para.text.split('-')
+                new_quote = QuoteModel(path, str(parse[0]), str(parse[1])
+                quotes.append(new_quote)
