c88f841f1cc2c9fd4eaf3ce0f48b7a6db8ea8ec7
Joseph Johnson <82980802+josephtjohnson@users.noreply.github.com>
2021-09-22 16:09:38 -0400

    Correct test_meme file path argument

--- test_MemeEngine.py
+++ test_MemeEngine.py
@@ -10,6 +10,6 @@ class TestMemeEngine(unittest.TestCase):
   def test_meme(self):
     with self.assertRaises(Exception):
       MemeEngine.make_meme(None, None, None, None)
-      MemeEngine.make_meme('/._data/photos/dog/xander_1.jpg', None, None, 501)
-      MemeEngine.make_meme('/._data/photos/dog/xander_1.jpg', None, None, None)
+      MemeEngine.make_meme('./_data/photos/dog/xander_1.jpg', None, None, 501)
+      MemeEngine.make_meme('./_data/photos/dog/xander_1.jpg', None, None, None)
     
