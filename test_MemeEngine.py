865744b456671b5d839e72b84890f7cf8eb55844
Joseph Johnson <82980802+josephtjohnson@users.noreply.github.com>
2021-09-22 16:03:55 -0400

    Create test_MemeEngine.py

---
+++ test_MemeEngine.py
@@ -0,0 +1,15 @@
+import unittest
+import QuoteEngine import MemeEngine
+
+class TestMemeEngine(unittest.TestCase):
+  
+  def test_init(self):
+    with self.assertRaises(Exception):
+      MemeEngine.__init__(None)
+    
+  def test_meme(self):
+    with self.assertRaises(Exception):
+      MemeEngine.make_meme(None, None, None, None)
+      MemeEngine.make_meme('/._data/photos/dog/xander_1.jpg', None, None, 501)
+      MemeEngine.make_meme('/._data/photos/dog/xander_1.jpg', None, None, None)
+    
