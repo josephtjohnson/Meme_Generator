ba6f59a8d4ca3d5d7ddd80c7eba8a0e4b6b3352e
Joseph Johnson <82980802+josephtjohnson@users.noreply.github.com>
2021-09-23 12:02:36 -0400

    Update imports

--- Ingestors/TxtIngestor.py
+++ Ingestors/TxtIngestor.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from typing import List
 from .IngestorInterface import Ingestor
-from .QuoteModel import QuoteModel
+from QuoteEngine import QuoteModel
 
 
 class TxtIngestor(Ingestor):
