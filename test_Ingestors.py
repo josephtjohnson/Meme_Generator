5da34e1c9e2044760c68bd5d9e1355155d7377df
Joseph Johnson <82980802+josephtjohnson@users.noreply.github.com>
2021-09-23 07:14:12 -0400

    Add test for data type

--- test_Ingestors.py
+++ test_Ingestors.py
@@ -3,6 +3,12 @@ from Ingestors import Ingestor
 
 class TestIngestors(unittest.TestCase):
   
+  def test_string(self):
+    types = [True, 1, 1.2,]
+    for type in types:
+      result = Ingestor.parse(type)
+      self.assertIsInstance(result, str)
+  
   def test_fail(self):
     file_types = ['.jpg', '.png', '.xml']
     for x in file_types:
@@ -13,7 +19,7 @@ class TestIngestors(unittest.TestCase):
     file_types = ['.pdf', '.csv', '.txt', '.docx'] 
     for x in file_types:
       result = Ingestor.parse(x)
-      self.assertTrue(result)
+      self.assertTrue(result) 
     
     
 if __name__ == '__main__':
