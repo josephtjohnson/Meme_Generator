00c7b6fb5068447969ed9a6caa4c2c9669cecb64
Joseph Johnson <johnson_jt@outlook.com>
2021-09-23 19:55:06 -0400

    Remove try-catch blocks due to inoperable code.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import Ingestor
+from Ingestor import Ingestor
 from MemeEngine import MemeEngine
 from QuoteModel import QuoteModel
 import argparse
@@ -10,53 +10,39 @@ def generate_meme(path=None, body=None, author=None):
     """ Generate a meme given an path and a quote """
     img = None
     quote = None
-    try:
-        if path is None:
-            images = "./_data/photos/dog/"
-            imgs = []
-            for root, dirs, files in os.walk(images):
-                imgs = [os.path.join(root, name) for name in files]
-            img = random.choice(imgs)
-        else:
-            img = path[0]
-    except FileNotFoundError:
-        print(f'Default image file location -- {images} -- not found.')
+    if path is None:
+        images = "./_data/photos/dog/"
+        imgs = []
+        for root, dirs, files in os.walk(images):
+            imgs = [os.path.join(root, name) for name in files]
+        img = random.choice(imgs)
     else:
-        pass
-    finally:
-        print('image finally statement executed')
+        img = path
 
-    try:
-        if body is None:
-            quote_files = ['./_data/DogQuotes/DogQuotesTXT.txt',
-                           './_data/DogQuotes/DogQuotesDOCX.docx',
-                           './_data/DogQuotes/DogQuotesPDF.pdf',
-                           './_data/DogQuotes/DogQuotesCSV.csv']
-            quotes = []
-            for f in quote_files:
-                quotes.extend(Ingestor.parse(f))
-
-            quote = random.choice(quotes)
-        else:
-            if author is None:
-                raise Exception('Author Required if Body is Used')
-
-    except FileNotFoundError:
-        print(f'Default quote file location -- {quote_files} -- not found.')
+    if body is None:
+        quote_files = ['./_data/DogQuotes/DogQuotesTXT.txt',
+                       './_data/DogQuotes/DogQuotesDOCX.docx',
+                       './_data/DogQuotes/DogQuotesPDF.pdf',
+                       './_data/DogQuotes/DogQuotesCSV.csv']
+        quotes = []
+        for file in quote_files:
+            quotes.extend(Ingestor.parse(file))
+        quote = random.choice(quotes)
     else:
+        if author is None:
+            raise Exception('Author Required if Body is Used')
         quote = QuoteModel(body, author)
-        meme = MemeEngine('./tmp')
-        path = meme.make_meme(img, quote.body, quote.author)
-        return path
-    finally:
-        print(' body/author finally statement executed')
+
+    meme = MemeEngine('./tmp')
+    path = meme.make_meme(img, quote.body, quote.author)
+    return path
 
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Generate a meme!')
-    parser.add_argument('--path', type=str, required=False,
-                        default=None, help='Insert path to image file')
+    parser.add_argument('--path', type=str, required=False, default=None,
+                        help='Insert path to image file')
     parser.add_argument('--body', type=str, required=False, default=None,
                         help='Insert quote text')
     parser.add_argument('--author', type=str, required=False,
