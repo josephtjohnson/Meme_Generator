b75cbe715e359f69912393ab66aa51b1519e1cb5
Joseph Johnson <82980802+josephtjohnson@users.noreply.github.com>
2021-09-23 10:33:37 -0400

    Add FileNotFoundError test case

--- test_meme.py
+++ test_meme.py
@@ -4,7 +4,8 @@ import meme
 class TestMeme(unittest.TestCase):
   
   def test_meme(self):
-    with self.assertRaise(Exception):
-      meme.generate_meme(None, None, None)
-      meme.generate_meme('./_data/photos/dog/xander_1.jpg', None, 'None')
+    self.assertRaise(Exception, meme.generate_meme, None, None, None)
+    self.assertRaises(FileNotFoundError, meme.generate_meme, None, None, None)
+      
+      
       
