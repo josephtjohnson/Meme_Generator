1461475dba1c59e784888587e5c818807de88851
Joseph Johnson <82980802+josephtjohnson@users.noreply.github.com>
2021-09-24 10:28:49 -0400

    Create __init__.py

---
+++ QuoteEngine/__init__.py
@@ -0,0 +1,7 @@
+from .QuoteModel import QuoteModel
+from .DocxIngestor import DocxIngestor
+from .CSVIngestor import CSVIngestor
+from .PDFIngestor import PDFIngestor
+from .TxtIngestor import TxtIngestor
+from .Ingestor import Ingestor
+from .IngestorInterface import IngestorInterface
