094389f98ce494db88e032bb87947f6e82bc287d
Joseph Johnson <82980802+josephtjohnson@users.noreply.github.com>
2021-09-28 09:36:14 -0400

    Add loop for ingestors

--- QuoteEngine/Ingestor.py
+++ QuoteEngine/Ingestor.py
@@ -8,12 +8,20 @@ from typing import List
 
 
 class Ingestor(IngestorInterface):
-    ingestors = (DocxIngestor, TxtIngestor, PDFIngestor, CSVIngestor)
+    ingestors = [DocxIngestor, TxtIngestor, PDFIngestor, CSVIngestor]
 
     @classmethod
     def parse(cls, path: str) -> List[QuoteModel]:
+        ext = path.split('.')[-1]
         for ingestor in cls.ingestors:
             if ingestor.can_ingest(path):
-                return ingestor.parse(path)
+                if ext = 'docx':
+                    return ingestors[0].parse(path)
+                elif ext = 'txt':
+                    return ingestors[1].parse(path)
+                elif ext = 'pdf':
+                    return ingestors[2].parse(path)
+                elif ext = 'csv':
+                    return ingestors[3].parse(path) 
             else:
-                raise Exception(f'{path} cannot be ingested by {ingestor}')
+                raise Exception(f'Unsupported file type: {ext}')
