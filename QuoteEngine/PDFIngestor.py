d3f63d7f3053b56a491c1d35db8d5a8165658c04
Joseph Johnson <82980802+josephtjohnson@users.noreply.github.com>
2021-09-29 11:34:21 -0400

    Add docstrings

--- QuoteEngine/PDFIngestor.py
+++ QuoteEngine/PDFIngestor.py
@@ -7,10 +7,33 @@ from .QuoteModel import QuoteModel
 
 
 class PDFIngestor(IngestorInterface):
+    """
+    A class that inherits from IngestorInterface and creates a list of quotes.
+    ...
+    Attributes
+    ----------
+    path : str
+        file path
+        
+    Methods
+    -------
+    parse(path):
+        Receives the quote file path and returns list of QuoteModel objects that contains the quote body and author.
+    """
+    
     file_types = ['pdf']
 
     @classmethod
     def parse(cls, path: str) -> List[QuoteModel]:
+        """
+        Parses an accepted file and returns a list of QuoteModel objects that contains the quote body and author.
+        
+        Parameters
+        ----------
+            path : str
+                file path
+        """        
+        
         if not cls.can_ingest(path):
             raise Exception(f'Cannot ingest {path}')
 
