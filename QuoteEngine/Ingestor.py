b34aa50314fd53cf3005f1c2008de756a31357b2
Joseph Johnson <82980802+josephtjohnson@users.noreply.github.com>
2021-09-29 10:37:26 -0400

    Add logging and try-catch blocks

--- QuoteEngine/Ingestor.py
+++ QuoteEngine/Ingestor.py
@@ -5,6 +5,22 @@ from .PDFIngestor import PDFIngestor
 from .IngestorInterface import IngestorInterface
 from .QuoteModel import QuoteModel
 from typing import List
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(message)s')
+
+file_handler = logging.FileHandler('ingestor.log')
+file_handler.setLevel(logging.ERROR)
+file_handler.setFormatter(formatter)
+
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
 
 
 class Ingestor(IngestorInterface):
@@ -12,15 +28,21 @@ class Ingestor(IngestorInterface):
 
     @classmethod
     def parse(cls, path: str) -> List[QuoteModel]:
-        ext = path.split('.')[-1].lower()
-        if not Ingestor.can_ingest(ext):
-            raise ValueError(f'Unsupported file type found: {ext}')
-        else:
-            if ext == 'txt':
-                return TxtIngestor.parse(path)
-            if ext == 'docx':
-                return DocxIngestor.parse(path)
-            if ext == 'pdf':
-                return PDFIngestor.parse(path)
-            if ext == 'csv':
-                return CSVIngestor.parse(path)
+        try:
+            ext = path.split('.')[-1].lower()
+        except ValueError:
+            logger.exception(f'{path} is not a valid file path')
+        try:
+            if not Ingestor.can_ingest(ext):
+                raise ValueError(f'Unsupported file type found: {ext}')
+            else:
+                if ext == 'txt':
+                    return TxtIngestor.parse(path)
+                if ext == 'docx':
+                    return DocxIngestor.parse(path)
+                if ext == 'pdf':
+                    return PDFIngestor.parse(path)
+                if ext == 'csv':
+                    return CSVIngestor.parse(path)
+        except Exception as e:
+            logger.exception(e)
