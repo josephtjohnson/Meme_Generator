81aaa56ec194d336861013ec957942928ea79a85
Joseph Johnson <82980802+josephtjohnson@users.noreply.github.com>
2021-09-29 11:35:09 -0400

    Add docstrings

--- QuoteEngine/CSVIngestor.py
+++ QuoteEngine/CSVIngestor.py
@@ -5,10 +5,33 @@ from .QuoteModel import QuoteModel
 
 
 class CSVIngestor(IngestorInterface):
+    """
+    A class that inherits from IngestorInterface and creates a list of quotes.
+    ...
+    Attributes
+    ----------
+    path : str
+        file path
+        
+    Methods
+    -------
+    parse(path):
+        Receives the quote file path and returns list of QuoteModel objects that contains the quote body and author.
+    """
+    
     file_types = ['csv']
 
     @classmethod
     def parse(cls, path: str) -> List[QuoteModel]:
+        """
+        Parses an accepted file and returns a list of QuoteModel objects that contains the quote body and author.
+        
+        Parameters
+        ----------
+            path : str
+                file path
+        """        
+        
         if not cls.can_ingest(path):
             raise Exception(f'Cannot ingest {path}')
         quotes = []
