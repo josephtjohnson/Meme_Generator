2c4c04e11c5ebf7e7ae7e392defa0450174e747b
Joseph Johnson <82980802+josephtjohnson@users.noreply.github.com>
2021-09-30 09:00:28 -0400

    Add functions
    Image resizing and text drawing functions were added.

--- helper/utils.py
+++ helper/utils.py
@@ -1,9 +1,11 @@
 from QuoteEngine import Ingestor, QuoteModel
 from MemeGenerator import MemeEngine
+from PIL import Image
 import logging
 import argparse
 import random
 import os
+impor textwrap
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -75,5 +77,65 @@ def open_image(category):
   except Exception as e:
     logger.exception(e)
 
+def image_resize(img_path, width=500):
+    """
+    Resize an image to be used by make_meme()
+    
+    Paramters
+    ---------
+        img_path : str
+            image file path
+        width : int
+            width of image in pixels (default = 500)
+    """
+
+    try:
+        with Image.open(img_path) as img:
+            ext = img_path.split('.')[-1]
+            try:
+                if width is not None and width <= 500:
+                    ratio = width/float(img.size[0])
+                    height = int(ratio*img.size[1])
+                    img = img.resize((width, height))
+                    return img
+                else:
+                    if width is None:
+                        raise Exception('Width zero')
+                    else:
+                        raise Exception('Width >500')
+            except Exception as e:
+                logger.exception(f'Width must be <500 but greater than zero: {e}')
+     except Exception as e:
+        logger.exception(e)
         
-        
+def text_draw(draw, text, author, fill, font):
+    """
+    Draw text in random location on image.
+    
+    Paramters
+    ---------
+        draw : image object
+            image
+        text : str
+            quote text
+        author : str
+            quote text
+        fill : tuple
+            text fill
+        font : font object
+            text font
+    """ 
+    
+    try:
+      draw = draw
+      x = random.randint(15,55)
+      y = random.randint(20,70)
+    except Exception:
+      logger.exception('Could not open image')
+    try:
+      text = textwrap.fill(text, 30)
+    except Exception:
+      logger.exception('Could not wrap text')
+    draw.text((x, y), text=text, fill=fill, font=font)
+    draw.text((x, y+20), text='-'+author, fill=fill, font=font)
+    return draw
